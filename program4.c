b8cc3d0f85f2a56220030808a21a3c0f6a787e37
saema-na <60117225+saema-na@users.noreply.github.com>
2021-03-02 10:41:36 -0500

    Update program4.c

--- program4.c
+++ program4.c
@@ -1,33 +1,8 @@
 /*
-Author: Saema Nazar
 Date:	4/30/19
 Purpose:	read a file containing 10 quiz scores for students in a class. Print the information from that file into 
          an output file containing student names, their 10 quiz scores, and score averages in a table format
-Sources	of	Help: class notes, lecture PPT's
 Time	Spent: 5 days
-----------------------------------
- Computing I -- COMP.1010 Honor Statement
- 
- The practice of good ethical behavior is essential for maintaining
- good order in the classroom, providing an enriching learning
- experience for students, and as training as a practicing computing
- professional upon graduation.  This practice is manifested in the
- University's Academic Integrity policy. Students are expected to
- strictly avoid academic dishonesty and adhere to the Academic
- Integrity policy as outlined in the course catalog.  Violations
- will be dealt with as outlined therein.
- 
- All programming assignments in this class are to be done by the
- student alone.  No outside help is permitted except the instructor
- and approved tutors.
- 
- I certify that the work submitted with this assignment is mine and
- was generated in a manner consistent with this document, the
- course academic policy on the course website on Blackboard, and
- the UMass Lowell academic code.
- 
- Date: 4/30/19
- Name: Saema Nazar
  */
 
 #include <stdio.h>
